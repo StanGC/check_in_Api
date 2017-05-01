894a1038b3c70b370e2e18ef3bcb2876b69878c1
Stan <solve153@gmail.com>
2017-05-01 16:39:17 +0800

    Implement user token

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root 'welcome#index'
 
+  namespace :api, :defaults => { :format => :json } do
+    namespace :v1 do
+
+    end
+  end
 end
