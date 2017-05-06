2436b1b1d106dc605c4108b7a66a53bc1726851d
Stan <solve153@gmail.com>
2017-05-06 15:53:22 +0800

    Implement follow users function

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,28 @@ class User < ApplicationRecord
   has_many :device
   mount_uploader :avatar, AvatarUploader
 
+  has_many :follower_relationships, class_name: "Relationship",
+                                     foreign_key: 'follower_id',
+                                     dependent: :destroy
+  has_many :followed_relationships, class_name: "Relationship",
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
+  has_many :following, through: :followed_relationships, source: :followed
+  has_many :followers, through: :follower_relationships, source: :follower
+
   def generate_authentication_token
     self.authentication_token = Devise.friendly_token
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
