9fbdae0e5ff63085e7c8247cb4fb73130379675d
Stan <solve153@gmail.com>
2017-05-06 12:17:25 +0800

    Add model test for device

---
+++ spec/models/device_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Device, type: :model do
+  describe "device model" do
+
+    it "is create" do
+      device = Device.new( :user_id => "2",
+                           :token => "zd8VVYVQpfzpLuini6Di",
+                           :operating_system => "ios",
+                           :version => "10.3",
+                           :name => "guest",
+                           :created_at => Time.now,
+                           :updated_at => Time.now,
+                           :avatar => "/uploads/user/avatar/2/image.png",
+                           )
+      expect( device ).to_not be_valid
+    end
+  end
+end
